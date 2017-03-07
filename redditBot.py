ea548c5dc84d051a7d4f02076dd5deb972b88a93
NickLargey <Nick_Largey@yahoo.com>
2017-03-06 22:39:51 -0500

    Update redditBot.py

--- redditBot.py
+++ redditBot.py
@@ -1,23 +1,27 @@
-#config file must be created with username, PW, and reddit app configs
 import praw
-import config
 import time
 import os
 
-def bot_login():
-	print("Logging in")
-	r = praw.Reddit(username = config.username,
-			password = config.password,
-			client_id = config.client_id,
-			client_secret = config.client_secret,
-			user_agent = "MyFriendGort v0.1")
+def authenticate():
+	print("Authenticating...")
+	reddit = praw.Reddit("klaatu", user_agent="My Friend Gort v2.0")
+	print("Authenticated as {}".format(reddit.user.me()))
 
-	return r
+	return reddit
 
-def run_bot(r, commentList):
+
+def main():
+	reddit = authenticate()
+	commentList = get_comments()
+
+	while True:
+		run_bot(reddit, commentList)
+
+
+def run_bot(reddit, commentList):
 	print("Checking for comments")
-	for comment in r.subreddit('test').comments(limit=25):
-		if "alien" in comment.body and comment.id not in commentList and comment.author != r.user.me():
+	for comment in reddit.subreddit('test').comments(limit=25):
+		if "alien" in comment.body and comment.id not in commentList and comment.author != reddit.user.me():
 			print("Comment " + comment.id + " found.") 
 			comment.reply("[Klaatu Nikto Barada](http://imgur.com/a/We8IR)")
 			commentList.append(comment.id)
@@ -32,6 +36,7 @@ def run_bot(r, commentList):
 		
 	time.sleep(10)
 
+
 def get_comments():
 	if not os.path.isfile("commentList.txt"):
 		commentList = []
@@ -44,8 +49,5 @@ def get_comments():
 	return commentList
 
 
-r = bot_login()
-commentList = get_comments()
-
-while True:
-	run_bot(r, commentList)
+if __name__ == "__main__":
+	main()
